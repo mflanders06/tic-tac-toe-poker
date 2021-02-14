5c3d4a456accb17e343cd0f0f4783f36c2a167b6
Michael Flanders <mflanders06@gmail.com>
2021-02-13 22:10:34 -0700

    Started part2

---
+++ Part2/index.js
@@ -0,0 +1,10 @@
+console.log('The house always wins!')
+
+let cardID = document.getElementById('cardID').value;
+let cardStyle = document.getElementById('cardColor').value;
+
+//console.log(cardID, cardStyle);
+
+function setCard(cardID, cardStyle){
+    console.log(cardID, cardStyle);
+}
